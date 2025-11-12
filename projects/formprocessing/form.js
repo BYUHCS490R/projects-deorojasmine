d0fc06985b63ccb9cdd089a42427716ada41dcc8
Jasmine Rio De Oro <jasmineriodeoro@email.com>
2025-11-11 19:48:21 -1000

    This week Project

---
+++ projects/formprocessing/form.js
@@ -0,0 +1,23 @@
+document.getElementById("myForm").addEventListener("submit", function(event){
+        event.preventDefault();
+        const first = document.getElementById("firstName").value;
+        const last = document.getElementById("lastName").value;
+        const age = document.getElementById("age").value;
+
+        if (!first || !last) {
+            alert("Please provide your full name.");
+            return
+        }
+        if (!first || !last) {
+            alert("You must be 18 years or older to submit this form");
+            return
+        }
+
+        const data = {
+            firstName: first,
+            lastName: last,
+            age: age
+        }
+
+        console.log(data);
+    });
